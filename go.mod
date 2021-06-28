e14d6e38440b1d64d54ca57fefa25271dbd310b4
Mike Christof <mhristof@gmail.com>
2021-06-28 19:09:04 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mhristof/go-precommit
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,19 @@
+package precommit
+
+type Repo struct {
+	Repo  string
+	Rev   string
+	Hooks []Hook
+}
+
+type Hook struct {
+	ID                      string
+	Exclude                 string   `yaml:"exclude,omitempty"`
+	Name                    string   `yaml:"name,omitempty"`
+	MinimumPreCommitVersion string   `yaml:"minimum_pre_commit_version,omitempty"`
+	Language                string   `yaml:"language,omitempty"`
+	AdditionalDependencies  []string `yaml:"additional_dependencies,omitempty"`
+	Entry                   string   `yaml:"entry,omitempty"`
+	Args                    []string `yaml:"args,omitempty"`
+	Types                   []string `yaml:"types,omitempty"`
+}
